090c265082c75d534f834e38a9eee2b0ffb70a69
Alex Smotritsky <alex.smotritsky@gmail.com>
2021-11-17 15:39:16 -0500

    cog

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+      const myData = res.data
+      console.log('myData:', myData)
+      setData(myData)
+    }
+
+    getData()
+    
+  }, [])
+
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <table>
+            <tr><td>USD</td></tr>
+            <tr><td>{data && data.bpi && data.bpi.USD != undefined && data.bpi['USD']['rate']}</td></tr>
+          </table>
+      
       </header>
     </div>
   );
